ad36339d07a74263233df65601f24237ec81a45d
xuexuexuease <2752037870@qq.com>
2019-05-24 13:53:03 +0800

    sendMessage

---
+++ src/main/java/com/controller/SendMessageController.java
@@ -0,0 +1,13 @@
+package com.controller;/**
+ * Created by 20190301 on 2019/5/24.
+ */
+
+/**
+ * @author Huxiaoxue
+ * @version V1.0
+ * @ClassName
+ * @desc: 发送消息
+ * @create: 2019-05-24
+ **/
+public class SendMessageController {
+}
